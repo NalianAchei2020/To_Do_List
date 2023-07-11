ec815058afb711185720a75cf8d4d28bca20398b
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-07-11 14:51:51 +0100

    add array

---
+++ src/index.js
@@ -0,0 +1,45 @@
+import './style.css';
+
+const task = [
+  {
+    description: 'wash the dishes',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'complete To Do list project',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Deploy the app to the server',
+    completed: false,
+    index: 3,
+  },
+
+];
+
+const renderTask = () => {
+  const taskList = document.getElementById('list');
+  taskList.innerHTML = '';
+  // sort tasks by index
+  task.sort((a, b) => a.index - b.index);
+
+  task.forEach((task) => {
+    const li = document.createElement('li');
+    li.classList = 'listItmes';
+    const check = document.createElement('input');
+    check.type = 'checkbox';
+    const span = document.createElement('span');
+    span.textContent = task.description;
+    li.appendChild(check);
+    check.appendChild(span);
+    const ellipsis = document.createElement('i');
+    ellipsis.classList.add('fa', 'fa-ellipsis-v');
+    ellipsis.setAttribute('aria-hidden', 'true');
+    li.appendChild(ellipsis);
+    taskList.appendChild(li);
+  });
+};
+
+window.addEventListener('load', renderTask);
